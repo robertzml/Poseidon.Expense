6be66efca0d0a35235b10e8f6cc49f5e43e8f8b7
robertzml <robertzml@126.com>
2017-04-23 23:07:35 +0800

    Add Poseidon.Expense.Test project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Expense.Test/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poseidon.Expense.Test
+{
+    using Poseidon.Winform.Base;
+    using Poseidon.Expense.ClientDx;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void menuExpenseAccount_Click(object sender, EventArgs e)
+        {
+            ChildFormManage.LoadMdiForm(this, typeof(FrmExpenseAccount));
+        }
+
+        private void menuExpenseReceipt_Click(object sender, EventArgs e)
+        {
+            ChildFormManage.LoadMdiForm(this, typeof(FrmExpenseReceipt));
+        }
+    }
+}
