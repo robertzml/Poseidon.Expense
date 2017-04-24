c5852d9d37511ee6dc73cab8bcfe6822d02fa265
robertzml <robertzml@126.com>
2017-04-24 08:55:08 +0800

    Delete form1.
    Signed-off-by: robertzml <robertzml@126.com>

--- Poseidon.Expense.ClientDx/Mod/ElectricReceipt.Designer.cs
+++ Poseidon.Expense.ClientDx/Mod/ElectricReceipt.Designer.cs
@@ -33,6 +33,7 @@
             this.lbElectricReceipt = new DevExpress.XtraEditors.ListBoxControl();
             this.bsElectricExpense = new System.Windows.Forms.BindingSource(this.components);
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.electricExpRecGrid = new Poseidon.Expense.ClientDx.ElectricExpenseRecordGrid();
             this.txtRemark = new DevExpress.XtraEditors.MemoEdit();
             this.btnDelete = new DevExpress.XtraEditors.SimpleButton();
             this.btnEdit = new DevExpress.XtraEditors.SimpleButton();
@@ -63,7 +64,6 @@
             this.layoutControlItem12 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem13 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem14 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.electricExpRecGrid = new Poseidon.Expense.ClientDx.ElectricExpenseRecordGrid();
             this.layoutControlItem15 = new DevExpress.XtraLayout.LayoutControlItem();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.lbElectricReceipt)).BeginInit();
@@ -155,6 +155,24 @@
             this.layoutControl1.Size = new System.Drawing.Size(754, 468);
             this.layoutControl1.TabIndex = 1;
             this.layoutControl1.Text = "layoutControl1";
+            // 
+            // electricExpRecGrid
+            // 
+            this.electricExpRecGrid.AllowFilter = true;
+            this.electricExpRecGrid.AllowGroup = true;
+            this.electricExpRecGrid.AllowSort = true;
+            this.electricExpRecGrid.DataSource = null;
+            this.electricExpRecGrid.Editable = false;
+            this.electricExpRecGrid.EnableMasterView = false;
+            this.electricExpRecGrid.Location = new System.Drawing.Point(12, 177);
+            this.electricExpRecGrid.Name = "electricExpRecGrid";
+            this.electricExpRecGrid.ShowAddMenu = false;
+            this.electricExpRecGrid.ShowFooter = false;
+            this.electricExpRecGrid.ShowLineNumber = true;
+            this.electricExpRecGrid.ShowMenu = false;
+            this.electricExpRecGrid.ShowNavigator = false;
+            this.electricExpRecGrid.Size = new System.Drawing.Size(730, 279);
+            this.electricExpRecGrid.TabIndex = 2;
             // 
             // txtRemark
             // 
@@ -467,22 +485,6 @@
             this.layoutControlItem14.Text = "备注";
             this.layoutControlItem14.TextSize = new System.Drawing.Size(82, 14);
             // 
-            // electricExpRecGrid
-            // 
-            this.electricExpRecGrid.AllowFilter = true;
-            this.electricExpRecGrid.AllowGroup = true;
-            this.electricExpRecGrid.AllowSort = true;
-            this.electricExpRecGrid.DataSource = null;
-            this.electricExpRecGrid.Editable = false;
-            this.electricExpRecGrid.Location = new System.Drawing.Point(12, 177);
-            this.electricExpRecGrid.Name = "electricExpRecGrid";
-            this.electricExpRecGrid.ShowAddMenu = false;
-            this.electricExpRecGrid.ShowFooter = false;
-            this.electricExpRecGrid.ShowLineNumber = true;
-            this.electricExpRecGrid.ShowNavigator = false;
-            this.electricExpRecGrid.Size = new System.Drawing.Size(730, 279);
-            this.electricExpRecGrid.TabIndex = 2;
-            // 
             // layoutControlItem15
             // 
             this.layoutControlItem15.Control = this.electricExpRecGrid;
@@ -492,12 +494,12 @@
             this.layoutControlItem15.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem15.TextVisible = false;
             // 
-            // ElectricExpenseReceipt
+            // ElectricReceipt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "ElectricExpenseReceipt";
+            this.Name = "ElectricReceipt";
             this.Size = new System.Drawing.Size(880, 474);
             this.Load += new System.EventHandler(this.ElectricExpenseReceipt_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
